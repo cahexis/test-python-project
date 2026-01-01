dd1b6b023ccdb866187b523f874ce8c07b10361d
cahexis <183315760+cahexis@users.noreply.github.com>
2026-01-01 12:59:00 -0600

    Finished project!!

--- Desktop/pythproj/pythfirstproj.py
+++ Desktop/pythproj/pythfirstproj.py
@@ -1,5 +1,15 @@
 #Funny project just as a test
-decision = input("Would you like to play better games?")
+import os
+import webbrowser
+from pathlib import Path
 
-if decision == "yes":
-    print("no thank you")
+confirmlist = ["yes", "YES", "yeah", "y", "Y"]
+pathtogame = Path("C:\Program Files (x86)\Steam\steamapps\common\Overwatch")
+decision = input("Would you like to play better games? ")
+url = "https://www.roblox.com/games/920587237/Adopt-Me"
+
+if decision in confirmlist:
+    if os.path.exists(pathtogame):
+        print("pretends to delete game")
+        webbrowser.open(url)
+
